18bf8543c0869b3d57354c0220906c1fab7841e2
anushmashaiju <anushma2015@gmail.com>
2024-01-08 23:35:59 +0530

    hosting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import { ToastContainer } from 'react-toastify';
 import './App.css';
-
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import 'react-toastify/dist/ReactToastify.css';
+import AddNewCourt from './Pages/AddNewCourt';
+import CourtUserViewPage from './Pages/CourtUserViewPage';
+import Mybookings from './Pages/Mybookings';
+import { AdminAuth, LoginAuth, UserAuth } from './Authorisation/authorization';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <ToastContainer/>  
+   <BrowserRouter>
+  <Routes>
+    <Route element={<LoginAuth/>}>
+    <Route path='/'element={<Login/>}/>
+    </Route>
+    {/*user route*/}
+    <Route element={<UserAuth/>}>
+    <Route path='/home' element={<Home/>}/>
+    <Route path='/courtUserViewPage/:id' element={<CourtUserViewPage/>}/>
+    <Route path='/myBookings' element={<Mybookings/>}/>
+     </Route>
+
+     {/*admin route*/}
+     <Route element={<AdminAuth/>}>
+    <Route path='/addNewCourt' element={<AddNewCourt/>}/>
+    </Route>
+  </Routes>
+   </BrowserRouter>
+   
+   </>
   );
 }
 
